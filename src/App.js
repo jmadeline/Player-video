601230f1b57a50ac72c3b4d7e6280006832ba15e
Madeline Joubert <madelinejoubert44@gmail.com>
2019-11-20 13:55:04 +0100

    video player

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactPlayer from 'react-player';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//https://www.youtube.com/watch?v=9zkwHIsiVoE
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      url: 'https://www.youtube.com/watch?v=9zkwHIsiVoE',
+      playing: false,
+      muted: false,
+      volume: 0.5,
+      progress: 0
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ url: event.target.value });
+  }
+
+  handlePlay = () => {
+    this.setState({ playing: !this.state.playing });
+  }
+
+  handleMute = () => {
+    this.setState({ muted: !this.state.muted });
+  }
+
+  handleVolumeUP = () => {
+    this.setState(
+      (prevState) => ({ ...prevState, volume: this.state.volume + 0.1 })
+    )
+  }
+
+  handleVolumeDown = () => {
+    this.setState(
+      (prevState) => ({ ...prevState, volume: this.state.volume - 0.1 })
+    )
+  }
+
+  handleProgress = (data) => {
+    this.setState({ progress: data.played.toFixed(2) * 100 });
+  }
+
+  changeProgress = (event) => {
+    const result = event.pageX / window.innerWidth;
+    this.player.seekTo(result);
+  }
+
+  ref = player => {
+    this.player = player;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input type="text" onChange={this.handleChange} />
+          <ReactPlayer
+            ref={this.ref}
+            url={this.state.url}
+            playing={this.state.playing}
+            muted={this.state.muted}
+            volume={this.state.volume}
+            onProgress={this.handleProgress}
+          />
+          <div className="progressBar" onClick={this.changeProgress}><div className="progress" style={{ width: this.state.progress + "%" }}></div></div>
+          <div className="buttons">
+            <button onClick={this.handlePlay}>Play / Pause</button>
+            <button onClick={this.handleMute}>Mute</button>
+            <button onClick={this.handleVolumeUP}>+</button>
+            <button onClick={this.handleVolumeDown}>-</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
